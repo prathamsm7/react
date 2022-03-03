29902a6340cef9fc87aeb5b98be78dbd340a79c0
prathamsm7 <79472183+prathamsm7@users.noreply.github.com>
2022-03-03 17:11:17 +0530

    updated

--- day-2/src/App.js
+++ day-2/src/App.js
@@ -6,27 +6,25 @@ function App() {
   let mM = ["Samsung", "HTC", "Micromax", " Apple"];
   return (
     <div className="App">
-      <h1>Mobile Operating System</h1>
-      {mOS.map((elem) => {
-        return MobileOS(elem);
-      })}
+      <ul id="os">
+        <h1>Mobile Operating System</h1>
+        {mOS.map((elem) => {
+          return MobileOS(elem);
+        })}
+      </ul>
 
-      <h1>Mobile Manufacturers</h1>
-      {mM.map((elem) => {
-        return MobileOS(elem);
-      })}
+      <ul id="mm">
+        <h1>Mobile Manufacturers</h1>
+        {mM.map((elem) => {
+          return MobileOS(elem);
+        })}
+      </ul>
     </div>
   );
 }
 
 function MobileOS(name) {
-  return (
-    <div>
-      <ul>
-        <li>{name}</li>
-      </ul>
-    </div>
-  );
+  return <li>{name}</li>;
 }
 
 export default App;
