8797245e47f4d5c5703c10eaf53c790fd1bcfb9d
prathamsm7 <79472183+prathamsm7@users.noreply.github.com>
2022-03-10 22:05:14 +0530

    init

---
+++ Day-7/forms/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/Form";
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <Users />
+    </div>
+  );
+}
+
+export default App;
